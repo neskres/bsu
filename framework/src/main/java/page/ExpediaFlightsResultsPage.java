f6791a64fccf6f2b92af27c6c2f7b40150dfa67a
Kristina Pozhoha <59255695+neskres@users.noreply.github.com>
2021-12-26 16:53:54 +0300

    Delete framework directory

--- framework/src/main/java/page/ExpediaFlightsResultsPage.java
+++
@@ -1,9 +0,0 @@
-package page;
-
-import org.openqa.selenium.WebDriver;
-
-public class ExpediaFlightsResultsPage extends AbstractPage{
-    protected ExpediaFlightsResultsPage(WebDriver driver) {
-        super(driver);
-    }
-}
--- framework/src/main/java/page/ExpediaStaysResultsPage.java
+++
@@ -1,9 +0,0 @@
-package page;
-
-import org.openqa.selenium.WebDriver;
-
-public class ExpediaStaysResultsPage extends AbstractPage {
-    protected ExpediaStaysResultsPage(WebDriver driver) {
-        super(driver);
-    }
-}
