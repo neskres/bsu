2403743568594ea3dd51c115610eed5cc1286bc4
Kristina Pozhoha <59255695+neskres@users.noreply.github.com>
2021-12-26 16:13:12 +0300

    Update ExpediaCarsPageSearchFormTest.java

--- framework/src/test/java/ExpediaCarsPageSearchFormTest.java
+++ framework/src/test/java/ExpediaCarsPageSearchFormTest.java
@@ -30,6 +30,6 @@ public class ExpediaCarsPageSearchFormTest extends CommonConditions {
                                                      .enterLocation(testLocation.getPickupLocation())
                                                      .searchCars();
 
-        Assert.assertTrue(resultsPage.isInitialized(testLocation.getPickupLocation()));
+        Assert.assertTrue(false);
     }
 }
